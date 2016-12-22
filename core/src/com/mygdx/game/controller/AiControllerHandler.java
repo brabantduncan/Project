4047854726fd6dec54258a193c82aba20d0d7a61
stijncoppens <stijn.coppens@student.howest.be>
2016-12-22 20:53:31 +0100

    bug fix

--- core/src/com/mygdx/game/controller/AiControllerHandler.java
+++ core/src/com/mygdx/game/controller/AiControllerHandler.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.mygdx.game.Bullet.BulletManager;
-import com.mygdx.game.Bullet.BulletManagerNukePet;
+import com.mygdx.game.Bullet.BulletManager;
 import com.mygdx.game.body.BodyBuilder;
 
 /**
