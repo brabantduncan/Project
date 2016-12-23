b781341b1050142ae96e4345f05c9f6452968c42
brabantduncan <duncan.brabant@student.howest.be>
2016-12-23 08:42:28 +0100

    enemybullet

---
+++ core/src/com/mygdx/game/Bullet/EnemyBullet.java
@@ -0,0 +1,37 @@
+package com.mygdx.game.Bullet;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.Body;
+
+/**
+ * Created by Gebruiker on 23/12/2016.
+ */
+public class EnemyBullet {
+
+
+    private Body b;
+    private Texture texture = new Texture("../assets/MinecraftIngots/chicken.png");
+
+
+    public EnemyBullet(Body b){
+        this.b = b;
+        editBody();
+
+    }
+
+    public void editBody(){
+        b.setUserData(this);
+        b.isBullet();
+    }
+
+    public Body getB() {
+        return b;
+    }
+
+
+    public Texture getTexture() {
+        return texture;
+    }
+
+
+}
